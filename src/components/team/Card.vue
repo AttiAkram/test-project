50fd2d467bbc6714ad20f21ea2abb59bb9da63c2
AttiAkram <akram.atti@iticopernico.it>
2024-01-30 13:01:16 +0100

    create a new template for Crad.vue

--- src/components/team/Card.vue
+++ src/components/team/Card.vue
@@ -3,8 +3,7 @@ export default {
   data() {
     return {
       people: [
-        {name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram', image: '.@/assets/img-team/user.png'},
-        {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
+        {name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram', image: './../../assets/img-team/user.png'},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
@@ -19,15 +18,25 @@ export default {
 </script>
 
 <template>
-  <main class="container">
-    <div class="row justify-content-center">
-      <div v-for="person in people" :key="person.name" class="card col-3 m-3" style="width: 18rem;">
-        <img :src="(person.image)" class="card-img-top" :alt="person.name">
-        <div class="card-body">
-          <p class="card-text">{{ person.name }} {{ person.age }} {{ person.descrizione }}</p>
+  <main class="container justify-content-center">
+    <div v-for="person in people" :key="person.name" class="card m-3">
+      <div class=" mb-3">
+        <div class="row g-0">
+          <div class="col-4">
+            <img src="./../../assets/img-team/user.png" class="img-fluid rounded-start my-4" :alt="person.name">
+          </div>
+          <div class="col-8">
+            <p class="card-body m-5"><h1>{{ person.name }}</h1>{{ person.age }} {{ person.descrizione }}</p>
+          </div>
         </div>
       </div>
+      <div  class="row g-0 mx-4">
+        <img src="./../../assets/img-team/user.png" class="col-4 m-4" alt="" style="width: 300px;">
+        <img src="./../../assets/img-team/user.png" class="col-4 m-4" alt="" style="width: 300px;">
+        <img src="./../../assets/img-team/user.png" class="col-4 m-4" alt="" style="width: 300px;">
+      </div>
     </div>
+
   </main>
 </template>
 
