caa77e686d8bfa7185b17960cd9ea8138e4f31b8
Semykopenko Ihor <aufrutten@gmail.com>
2024-01-30 12:28:15 +0100

    resovle issue

--- src/components/team/Card.vue
+++ src/components/team/Card.vue
@@ -3,7 +3,7 @@ export default {
   data() {
     return {
       people: [
-        {name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram', image: '.@/assets/img-team/user.png'},
+        {name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram', image: "/user.png"},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
         {name: 'Paolo', age: 32, descrizione: 'Ciao, sono paolo', image: './immagini/paolo.jpg'},
@@ -22,7 +22,7 @@ export default {
   <main class="container">
     <div class="row justify-content-center">
       <div v-for="person in people" :key="person.name" class="card col-3 m-3" style="width: 18rem;">
-        <img :src="(person.image)" class="card-img-top" :alt="person.name">
+        <img :src="person.image" class="card-img-top" :alt="person.name">
         <div class="card-body">
           <p class="card-text">{{ person.name }} {{ person.age }} {{ person.descrizione }}</p>
         </div>
