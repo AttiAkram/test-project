1d923dd62fa12eec7cdd5921d2c02113e8c2a2f4
AttiAkram <akram.atti@iticopernico.it>
2024-02-01 11:12:28 +0100

    fix problem whit img

--- src/components/team/Card.vue
+++ src/components/team/Card.vue
@@ -4,32 +4,32 @@ export default {
     return {
       people: [
         {
-          name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram ', user: '/img-team/Akram/user.png', userimg: [
-            {img1: '/img-team/Akram/user.png'},
-            {img2: '/img-team/Akram/user.png'},
-            {img3: '/img-team/Akram/user.png'},
-          ]
+          name: 'Akram', age: 17, descrizione: 'Ciao, sono Akram ', user: '/img-team/Akram/user.png', userimg: {
+            img1: '/img-team/Akram/user.png',
+            img2: '/img-team/Akram/user.png',
+            img3: '/img-team/Akram/user.png',
+          }
         },
         {
-          name: 'Claudio', age: 38, descrizione: 'Ciao, sono Claudio', user: '/img-team/Claudio/user.png', userimg: [
-            {img1: '/img-team/Claudio/user.png'},
-            {img2: '/img-team/Claudio/user.png'},
-            {img3: '/img-team/Claudio/user.png'},
-          ]
+          name: 'Marco', age: 23, descrizione: 'Ciao, sono Marco', user: '/img-team/Marco/user.png', userimg: {
+            img1: '/img-team/Marco/user.png',
+            img2: '/img-team/Marco/user.png',
+            img3: '/img-team/Marco/user.png',
+          }
         },
         {
-          name: 'Marco', age: 23, descrizione: 'Ciao, sono Marco', user: '/img-team/Marco/user.png', userimg: [
-            {img1: '/img-team/Marco/user.png'},
-            {img2: '/img-team/Marco/user.png'},
-            {img3: '/img-team/Marco/user.png'},
-          ]
+          name: 'Claudio', age: 23, descrizione: 'Ciao, sono Claudio', user: '/img-team/Claudio/user.png', userimg: {
+            img1: '/img-team/Claudio/user.png',
+            img2: '/img-team/Claudio/user.png',
+            img3: '/img-team/Claudio/user.png',
+          }
         },
         {
-          name: 'Paolo', age: 47, descrizione: 'Ciao, sono Paolo', user: '/img-team/Paolo/user.png', userimg: [
-            {img1: '/img-team/Paolo/user.png'},
-            {img2: '/img-team/Paolo/user.png'},
-            {img3: '/img-team/Paolo/user.png'},
-          ]
+          name: 'Paolo', age: 47, descrizione: 'Ciao, sono Paolo', user: '/img-team/Paolo/user.png', userimg: {
+            img1: '/img-team/Paolo/user.png',
+            img2: '/img-team/Paolo/user.png',
+            img3: '/img-team/Paolo/user.png',
+          }
         },
       ]
     };
@@ -52,9 +52,9 @@ export default {
         </div>
         <div class="container justify-content-center">
           <div class="row overflow-hidden g-0 mx-4  d-none d-lg-flex custom-lg-flex">
-            <img :src="'/img-team/Paolo/user.png'" class="col-3 m-4" alt="">
-            <img :src="'/img-team/Paolo/user.png'" class="col-3 m-4" alt="">
-            <img :src="'/img-team/Paolo/user.png'" class="col-3 m-4" alt="">
+            <img :src="person.userimg.img1" class="col-3 m-4" alt="">
+            <img :src="person.userimg.img2" class="col-3 m-4" alt="">
+            <img :src="person.userimg.img3" class="col-3 m-4" alt="">
           </div>
         </div>
       </div>
